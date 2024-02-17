22d75047d55b3ce6d0a9d09748b1b3730dddbe6e
04stefke <jovanovics399@yahoo.com>
2024-02-17 13:53:34 +0000

    added firebase in the project

---
+++ messanger-app/src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/app'
+const firebaseConfig = {
+  apiKey: "AIzaSyD_HaI1zgSpwjCDtojHPsyp91dyuUjQXKo",
+  authDomain: "allchat-f72cf.firebaseapp.com",
+  projectId: "allchat-f72cf",
+  storageBucket: "allchat-f72cf.appspot.com",
+  messagingSenderId: "817962144650",
+  appId: "1:817962144650:web:c505fdf4591284b77dae73"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
