dacbade5adfb67f94a1298d6ce62174fb7028f50
04stefke <jovanovics399@yahoo.com>
2024-02-17 16:08:18 +0000

    starting making users using emails and passwords

--- messanger-app/src/components/Register/Register.jsx
+++ messanger-app/src/components/Register/Register.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import {FileAddOutlined} from '@ant-design/icons'
 import './Register.scss'
+import {auth} from '../../firebase'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+
 const Register = () => {
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+    
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+        
+    }catch(err){
+      console.log(err)
+    }
+    
+
+  }
   return (
     <div className='formContainer'>
         <div className="formWrapper">
             <h1>My Chat</h1>
             <h3>Register</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Please Enter Your Name' />
                 <input type="email" placeholder='Please enter your email' />
                 <input type="password" placeholder='Please enter your password' />
--- messanger-app/src/firebase.js
+++ messanger-app/src/firebase.js
@@ -1,14 +1,15 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/app'
+import {getAuth} from 'firebase/auth'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD_HaI1zgSpwjCDtojHPsyp91dyuUjQXKo",
-  authDomain: "allchat-f72cf.firebaseapp.com",
-  projectId: "allchat-f72cf",
-  storageBucket: "allchat-f72cf.appspot.com",
-  messagingSenderId: "817962144650",
-  appId: "1:817962144650:web:c505fdf4591284b77dae73"
-};
+    apiKey: "AIzaSyD_HaI1zgSpwjCDtojHPsyp91dyuUjQXKo",
+    authDomain: "allchat-f72cf.firebaseapp.com",
+    projectId: "allchat-f72cf",
+    storageBucket: "allchat-f72cf.appspot.com",
+    messagingSenderId: "817962144650",
+    appId: "1:817962144650:web:c505fdf4591284b77dae73"
+  };
 
 
 export const app = initializeApp(firebaseConfig);
