cdbbd1fe8505f8ed1f1fd9836a9e3a802ff14b8f
04stefke <jovanovics399@yahoo.com>
2024-02-19 18:07:27 +0000

    changed the profile pic in the navbar and the name of the user

--- messanger-app/src/components/Home/features/Navbar.jsx
+++ messanger-app/src/components/Home/features/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {UserOutlined} from '@ant-design/icons'
 import {auth} from '../../../firebase'
 import {signOut} from 'firebase/auth'
+import {AuthContext} from '../../../context/AuthContext'
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
   return (
     <div className='navbarContainer'>
       <span>Stefan's Chat</span>
       <div className="user">
-        <UserOutlined />
-        <span className='userName'>Stefan</span>
+        <img src={currentUser.photoURL} style={{width: '40px', borderRadius: '50%'}}/> 
+        <span className='userName'>{currentUser.displayName}</span>
         <button className='btn' onClick={() => signOut(auth)}>Logout</button>
       </div> 
 
