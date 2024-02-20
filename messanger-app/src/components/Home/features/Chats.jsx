4a859bb2c03d84a96f5043a26740190e63c2e133
04stefke <jovanovics399@yahoo.com>
2024-02-20 14:41:51 +0000

    connected the chats to database

--- messanger-app/src/components/Home/features/Chats.jsx
+++ messanger-app/src/components/Home/features/Chats.jsx
@@ -1,16 +1,37 @@
 import { UserOutlined } from '@ant-design/icons'
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useState } from 'react'
+import { db } from '../../../firebase'
+import { AuthContext } from '../../../context/AuthContext'
 
 const Chats = () => {
+  const [chats, setChats] = useState([])
+  const {currentUser} = useContext(AuthContext)
+  useEffect(() => {
+    const getChats =() => {
+      const unsub = onSnapshot(doc(db, "userChat", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      })
+      return() => {
+        unsub()
+      }
+      
+    }
+    currentUser.uid && getChats()
+  },[currentUser.uid])
+  console.log(Object.entries(chats))
   return (
     <div className='chatsContainer'>
-      <div className='userChat'>
-        <UserOutlined/>
-        <div className="userChatInfo">
-          <span>Stefan</span>
-          <p>this is message</p>
+      {Object.entries(chats)?.map(chat => ( 
+        <div className='userChat' key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} width={'50px'} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+        ))}
+      
     </div>
   )
 }
