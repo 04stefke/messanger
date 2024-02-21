b0d8a83a8d6af99122171f06b063b20f006a1982
04stefke <jovanovics399@yahoo.com>
2024-02-21 18:34:16 +0000

    deleted the just seen text

--- messanger-app/src/components/Home/features/Message.jsx
+++ messanger-app/src/components/Home/features/Message.jsx
@@ -28,7 +28,7 @@ const Message = ({message}) => {
     <div className={`message ${message?.senderId === currentUser.uid && 'owner'}`}>
       <div className="messageInfo">
           <img src={message?.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
-          <span>just now</span>
+          
       </div>
       <div ref={ref} className="messageContent">
         <p>{message.text}</p>
