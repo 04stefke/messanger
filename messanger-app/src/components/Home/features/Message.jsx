eaa6434af41009a2a12eacaefd26a71b7b809dd1
04stefke <jovanovics399@yahoo.com>
2024-02-21 17:45:48 +0000

    added lightbox for the pictures that are sent in the chat

--- messanger-app/src/components/Home/features/Message.jsx
+++ messanger-app/src/components/Home/features/Message.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { AuthContext } from '../../../context/AuthContext'
 import { UserContext } from '../../../context/UserContext'
 const Message = ({message}) => {
+  const [lightbox, setLighbox] = useState(false)
   const {currentUser} = useContext(AuthContext)
   const {data} = useContext(UserContext)
   console.log(message)
@@ -14,17 +15,27 @@ const Message = ({message}) => {
   if (!message || Object.keys(message).length === 0) {
     return <div>Loading message...</div>;
   }
+
+  const handleLightbox = () => {
+    if(lightbox){
+      setLighbox(false)
+    } else{
+      setLighbox(true)
+    }
+  }
   
   return (
     <div className={`message ${message?.senderId === currentUser.uid && 'owner'}`}>
       <div className="messageInfo">
-           
           <img src={message?.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
           <span>just now</span>
       </div>
       <div ref={ref} className="messageContent">
         <p>{message.text}</p>
-        {message.img && <img src={message.img}  alt="" />}
+        <div className={`lightbox ${lightbox && 'imgLightboxx'}`}>
+          {message.img && <img src={message.img} className={`${lightbox && 'imgLightbox'}`} onClick={handleLightbox} alt="" />}
+        </div>
+        
       </div>
       
     </div>
